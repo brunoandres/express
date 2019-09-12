b52a4ad399bc765f00d2cdbdcdbc534e6be35cf0
brunoandres <brunoandres2013@gmail.com>
2019-09-12 15:06:35 -0300

    firs commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import mongoose from 'mongoose';
+import router from './routes';
+
+//const express = require('express');
+//const morgan = require('morgan');
+//const cors = require('cors');
+
+//Conexion a la base de datos
+mongoose.Promise = global.Promise;
+const dbUrl = 'mongodb://localhost:27017/dbsistema';
+mongoose.connect(dbUrl, {useCreateIndex:true,useNewUrlParser: true})
+.then(mongoose => console.log('Conectado a la BD en el puerto 27017'))
+.catch(err => console.log(err));
+const app = express();
+
+app.set('port',process.env.PORT || 3000);
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/api',router);
+
+app.listen(app.get('port'),()=>{
+    console.log("server ejecutandose en puerto "+ app.get('port')); 
+});
+
---
+++ models/index.js
@@ -0,0 +1,5 @@
+import Categoria from './categoria';
+import Articulo from './articulo';
+export default {
+    Categoria, Articulo
+}
